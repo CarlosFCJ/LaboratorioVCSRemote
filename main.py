985e63832c951c16a4b93fd1bd7053b637ba1059
CarlosFCJ <62518018+CarlosFCJ@users.noreply.github.com>
2020-03-25 08:30:01 -0500

    Mi primer commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 24 13:59:42 2020
+
+@author: Carlos_Carreño
+"""
+
+a = int(input("ingrese el numero a: "))
+c = int(input("Ingrese el numero c: "))
+
+print("El productos de los dos numeros es " + str(a*c))
+
+print("El doble del numero a es " + str(a*2))
