8b3eb9486c4d0a47fae823ca0bb613ee3b7d81b1
CarlosFCJ <62518018+CarlosFCJ@users.noreply.github.com>
2020-03-25 08:45:46 -0500

    Mi primer modificacion

--- main.py
+++ main.py
@@ -4,10 +4,14 @@ Created on Tue Mar 24 13:59:42 2020
 
 @author: Carlos_Carreño
 """
-
 a = int(input("ingrese el numero a: "))
 c = int(input("Ingrese el numero c: "))
 
 print("El productos de los dos numeros es " + str(a*c))
-
 print("El doble del numero a es " + str(a*2))
+
+b = int(input("Ingrese el numero b: "))
+print("El cuadrado del numero b es " + str(b**2))
+
+d = int(input("Ingrese el numero d: "))
+print("La raiz cuadrado del numero d es " + str(d**1/2))
